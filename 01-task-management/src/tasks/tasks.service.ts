43f4c6f4853c536f2f7cb88986548e323aa15cb8
Daniel Florez <danielflorez111@hotmail.com>
2020-07-20 16:31:33 -0500

    Adding createTask method

--- 01-task-management/src/tasks/tasks.service.ts
+++ 01-task-management/src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-import { Task } from './task.model';
+import { v1 as uuid } from 'uuid';
+
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
@@ -8,5 +10,18 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        };
+
+        this.tasks.push(task);
+
+        return task;
     }
 }
