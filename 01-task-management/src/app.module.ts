0578b002a78a8240fec81b5e290119b7dbfa9b88
Daniel Florez <danielflorez111@hotmail.com>
2020-08-02 21:52:46 -0500

    SignUp, SignIn, Auth guard - Part 1

--- 01-task-management/src/app.module.ts
+++ 01-task-management/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
 import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(typeOrmConfig),
-        TasksModule
+        TasksModule,
+        AuthModule
     ],
 })
 export class AppModule { }
