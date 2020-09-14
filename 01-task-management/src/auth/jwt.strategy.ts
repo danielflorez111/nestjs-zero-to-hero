2beb109dbff05f45090fa799424d5a298db77054
Daniel Florez <danielflorez111@hotmail.com>
2020-09-13 23:57:03 -0500

    Production configuration

--- 01-task-management/src/auth/jwt.strategy.ts
+++ 01-task-management/src/auth/jwt.strategy.ts
@@ -3,10 +3,13 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { Strategy, ExtractJwt } from 'passport-jwt';
+import * as config from 'config';
 
 import { JwtPayload } from './dto/jwt-payload.interface';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
+
+const jwtConfig = config.get('jwt');
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -17,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'topSecret51'
+            secretOrKey: process.env.JWT_SECRET || jwtConfig.secret
         });
     }
 
