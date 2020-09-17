66cf1dde2f8797d5f17fbdda7678151da0f2c4df
Daniel Florez <danielflorez111@hotmail.com>
2020-09-16 23:56:58 -0500

    Enabling Cors in production

--- 01-task-management/src/main.ts
+++ 01-task-management/src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
 
     if (process.env.NODE_ENV === 'development') {
         app.enableCors();
+    } else {
+        app.enableCors({ origin: serverConfig.origin });
     }
 
     await app.listen(port);
